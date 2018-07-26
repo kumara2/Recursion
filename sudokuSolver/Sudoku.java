f3941fb2f41aef18d054606482358cae3d5acc4a
Akhilesh Kumar <kumara2@miamioh.edu>
2018-07-25 21:01:27 -0400

    sudoku

---
+++ sudokuSolver/Sudoku.java
@@ -0,0 +1,18 @@
+package sudokuSolver;
+import java.util.*;
+
+public class Sudoku {
+
+	/*
+	 * Refer to https://leetcode.com/problems/sudoku-solver/description/
+	 * https://leetcode.com/submissions/detail/165842097/
+	 * Solution on Leetcode and pramp
+	 */
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
